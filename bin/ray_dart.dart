260c0c6f77b5d56aeef4be757ead305ba49f3b31
Tudo12 <gfirman923@gmail.com>
2021-10-04 11:42:15 +0700

    Add before command handler

--- bin/ray_dart.dart
+++ bin/ray_dart.dart
@@ -1,7 +1,8 @@
 // ignore_for_file: unused_shown_name, avoid_single_cascade_in_expression_statements
 
+// import 'dart:js';
 
-import "package:nyxx/nyxx.dart" ;
+import "package:nyxx/nyxx.dart";
 import "package:nyxx_commander/commander.dart";
 import "package:ray_dart/ray_dart.dart" as rod;
 import 'package:dotenv/dotenv.dart' show load, clean, isEveryDefined, env;
@@ -12,10 +13,28 @@ import "package:nyxx_interactions/interactions.dart"
         Interactions,
         SlashCommandBuilder;
 
+bool beforeHandler(CommandContext context) {
+  if (context.author.bot) {
+     return false;
+   }
+  if (context.channel is DMChannel) {
+    context.channel.sendMessage(MessageBuilder.content(
+      "The bot doesn't support direct messages, please use it on a server!",
+    ));
+    return false;
+  }
+  return true;
+}
+
 void main() {
   load();
+
   final bot = Nyxx("${env['BOT_TOKEN']}", GatewayIntents.allUnprivileged);
-  Commander(bot, prefix: "${env['PREFIX']}")
+  Commander(
+    bot,
+    prefix: "${env['PREFIX']}",
+    beforeCommandHandler: beforeHandler,
+  )
     ..registerCommand("info", rod.infoCommand)
     ..registerCommand("help", rod.helpCommand)
     ..registerCommand("say", rod.sayCommand)
@@ -28,4 +47,3 @@ void main() {
       ..registerHandler(rod.pingSlashHandler))
     ..syncOnReady();
 }
-
